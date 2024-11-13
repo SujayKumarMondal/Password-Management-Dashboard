d10d3ff35f808db4911e092c49b26045ac69ba11
SujayKumarMondal <sujay@surelia.info>
2024-11-13 09:13:17 +0530

    Added password validation logic

--- loginapp/forms.py
+++ loginapp/forms.py
@@ -1,36 +1,57 @@
+import re
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
+import regex
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms import validators
+import wtforms
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from loginapp.models import User
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
+
+
+def password_validator(form, field):
+    """ Custom password validator to enforce password rules. """
+    password = field.data
+    # Minimum 6 characters, maximum 20 characters
+    if len(password) < 6 or len(password) > 20:
+        raise ValidationError("Password must be between 6 and 20 characters.")
+
+    # Must contain at least one number
+    if not re.search(r'\d', password):
+        raise ValidationError("Password must contain at least one number.")
+
+    # Must contain at least one special character (excluding restricted ones)
+    if not re.search(r'[!@#$%^&*()_+={}[\]:;"\'<>,.?\\|`~]', password):
+        raise ValidationError("Password must contain at least one special character.")
+
+    # Must not contain `_`, `/`, or `,`
+    if re.search(r'[_/\,]', password):
+        raise ValidationError("Password cannot contain '_', '/', or ','.")
 
 class RegistrationForm(FlaskForm):
-    """
-    Registration form 
-    Elements containing:
-    1. name
-    2. email
-    3. password
-    4. confirm password
-    5. submit button
-    """
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
-    
+    name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-
-    password = PasswordField('Password', validators=[DataRequired()])
-
-    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
-
-    submit_btn = SubmitField('Register')
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=6, max=20),
+        Regexp(r'^(?!.*[_,/]).*$', message="Password cannot contain _, / or ,"),  # Disallow _, /, and ,
+        Regexp(r'.*[A-Za-z].*', message="Password must contain at least one letter"),
+        Regexp(r'.*\d.*', message="Password must contain at least one number"),
+        Regexp(r'.*[\W_].*', message="Password must contain at least one special character"),  # Special character
+    ])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password', message="Passwords must match")
+    ])
+    submit = SubmitField('Sign Up')
 
     # Adding validation for abnormal anomalies
     def validate_email(self, email):
-        test_condition = User.query.filter_by(email = email.data).first()
+        test_condition = User.query.filter_by(email=email.data).first()
         if test_condition:
-            raise ValidationError('Email already exist in database.')
+            raise ValidationError('Email already exists in the database.')
 
 class LoginForm(FlaskForm):    
     """
@@ -48,7 +69,7 @@ class LoginForm(FlaskForm):
 
 
 class AddPassword(FlaskForm):
-    """ Form for add page. Where user will input therir credintial in password manager. """
+    """ Form for add page. Where user will input their credential in password manager. """
     webaddress = StringField('Web Address', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -62,12 +83,12 @@ class RequestResetForm(FlaskForm):
     submit_btn = SubmitField('Request Password Reset')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email = email.data).first()
+        user = User.query.filter_by(email=email.data).first()
         if user is None:
             raise ValidationError('There is no account with this email. You must register first.')
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), password_validator])
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
 
     submit_btn = SubmitField('Reset Password')
@@ -82,15 +103,20 @@ class UserAccountUpdate(FlaskForm):
     # Adding validation for abnormal anomalies
     def validate_email(self, email):
         if email.data != current_user.email:
-            test_condition = User.query.filter_by(email = email.data).first()
+            test_condition = User.query.filter_by(email=email.data).first()
             if test_condition:
-                raise ValidationError('Email already exist in database.')
+                raise ValidationError('Email already exists in the database.')
 
 class UpdatePassword(FlaskForm):
-    """ Form for Update page. Where user will update their credintial in password manager. """
-    webaddress = StringField('Web Address', validators=[DataRequired()])
+    webaddress = StringField('Website Address', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
-    submit_btn = SubmitField('Update')
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=6, max=20),
+        Regexp(r'^(?!.*[_,/]).*$', message="Password cannot contain _, / or ,"),
+        Regexp(r'.*[A-Za-z].*', message="Password must contain at least one letter"),
+        Regexp(r'.*\d.*', message="Password must contain at least one number"),
+        Regexp(r'.*[\W_].*', message="Password must contain at least one special character"),
+    ])
+    submit_btn = SubmitField('Update Password')
